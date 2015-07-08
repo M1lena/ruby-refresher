0801a84a0449ddbb8da34e3014a91b4277de4825
M1lena <stoevami@gmail.com>
2015-07-08 13:18:50 +0200

    27 pending

--- lib/questions.rb
+++ lib/questions.rb
@@ -74,6 +74,9 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
+  c= []
+  c.push(Array(array).select{ |x| x.even? })
+  c.push(Array(array).select{ |x| x.odd? })
 end
 
 # count the numbers of elements in an element which are palindromes
